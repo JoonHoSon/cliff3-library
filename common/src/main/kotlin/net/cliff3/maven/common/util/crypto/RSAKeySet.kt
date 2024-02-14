76e3feefe59e7c8e290a8cd45e54acd9bf0f53de
JoonHo Son <joonho.son@me.com>
2024-02-14 10:00:08 +0900

    fix: 지수/계수를 이용한 생성자 함수에서 오류 수정
    공개키 지수 생성부의 인자를 공개키 계수로 잘못 설정한 부분 수정

--- common/src/main/kotlin/net/cliff3/maven/common/util/crypto/RSAKeySet.kt
+++ common/src/main/kotlin/net/cliff3/maven/common/util/crypto/RSAKeySet.kt
@@ -67,13 +67,15 @@ class RSAKeySet {
      * 공개/개인키 및 각 계수(modulus) 및 지수(exponent)의 문자열(16진수 형태) 처리 여부. 해당 값이 `true`일 경우 16진수 형태의
      * 공개/개인키 및 각 계수 및 지수를 생성한다.
      */
-    var isKeyToHexString: Boolean = false
+    var isMakeExtra: Boolean = false
         private set
 
     /**
      * 암호회 처리된 결과
      */
     var encryptedValue: ByteArray? = null
+
+    private val encoder = Base64.getEncoder()
 
     /**
      * Constructor
@@ -83,18 +85,17 @@ class RSAKeySet {
      * @param 계수(modulus) 및 지수(exponent) 16진수 문자열 생성 여부
      */
     @Throws(CryptoException::class)
-    constructor(publicKey: Key, privateKey: Key, isKeyToHexString: Boolean) {
+    constructor(publicKey: Key, privateKey: Key, makeExtra: Boolean = false) {
         this.publicKey = publicKey
         this.privateKey = privateKey
-        this.isKeyToHexString = isKeyToHexString
+        this.isMakeExtra = makeExtra
 
-        if (!isKeyToHexString) {
+        if (!makeExtra) {
             return
         }
 
         try {
             val factory: KeyFactory = KeyFactory.getInstance("RSA")
-            val encoder: Base64.Encoder = Base64.getEncoder()
 
             // public key
             val publicSpec: RSAPublicKeySpec = factory.getKeySpec(publicKey, RSAPublicKeySpec::class.java)
@@ -127,7 +128,8 @@ class RSAKeySet {
         publicKeyModulus: String,
         publicKeyExponent: String,
         privateKeyModulus: String,
-        privateKeyExponent: String
+        privateKeyExponent: String,
+        makeEncodedString: Boolean = false
     ) {
         this.publicKeyModulus = publicKeyModulus
         this.publicKeyExponent = publicKeyExponent
@@ -139,17 +141,24 @@ class RSAKeySet {
 
             // public key 생성
             var modulus: BigInteger = BigInteger(publicKeyModulus, 16)
-            var exponent: BigInteger = BigInteger(publicKeyModulus, 16)
+            var exponent: BigInteger = BigInteger(publicKeyExponent, 16)
             val publicSpec: RSAPublicKeySpec = RSAPublicKeySpec(modulus, exponent)
 
-            this.publicKey = factory.generatePublic(publicSpec)
+            publicKey = factory.generatePublic(publicSpec)
 
             // private key 생성
             modulus = BigInteger(privateKeyModulus, 16)
             exponent = BigInteger(privateKeyExponent, 16)
             val privateSpec: RSAPrivateKeySpec = RSAPrivateKeySpec(modulus, exponent)
 
-            this.privateKey = factory.generatePrivate(privateSpec)
+            privateKey = factory.generatePrivate(privateSpec)
+
+            if (!makeEncodedString) {
+                return
+            }
+
+            this.publicKeyString = encoder.encodeToString(publicKey.encoded)
+            this.privateKeyString = encoder.encodeToString(privateKey.encoded)
         } catch (e: Throwable) {
             throw CryptoException("계수 및 지수로부터 키 생성 실패", e)
         }
